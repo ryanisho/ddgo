bb4f19c6b589caa8816928df3089cf2d23285f04
Ryan Ho <rh564@cornell.ed>
2024-11-08 18:01:11 -0500

    initial commit v3

--- go.mod
+++
@@ -1,8 +0,0 @@
-module ddgo
-
-go 1.21
-
-require (
-    github.com/mattn/go-sqlite3 v1.14.17
-    github.com/shirou/gopsutil/v3 v3.23.11
-)
--- internal/api/routes.go
+++
@@ -1,8 +0,0 @@
-package api
-
-func (s *Server) routes() {
-    s.mux.HandleFunc("/api/metrics", s.handleMetrics)
-    s.mux.HandleFunc("/api/alerts", s.handleAlerts)
-    s.mux.HandleFunc("/api/alerts/history", s.handleAlertHistory)
-    s.mux.Handle("/", http.FileServer(http.Dir("web/dashboard")))
-}
